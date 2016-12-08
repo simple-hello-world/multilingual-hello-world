fbca556072706cae5ea09d95695453921038e665
Sergio Sanchez <sanchezs@vmware.com>
2016-12-08 14:58:11 +0200

    Added Darwin support
    Added Darwin support.

--- bash-depends-on-all/hello-world-from-all.sh
+++ bash-depends-on-all/hello-world-from-all.sh
@@ -2,4 +2,5 @@
 rm -rf ../build
 mkdir ../build
 java -jar ../java-hello-world/target/java-hello-world-1.0.0-SNAPSHOT.jar > ../build/output.txt
-../go-hello-world/bin/go-hello-world-amd64 >> ../build/output.txt
+../go-hello-world/bin/go-hello-world-linux-amd64 >> ../build/output.txt
+#../go-hello-world/bin/go-hello-world-darwin-amd64 >> ../build/output.txt

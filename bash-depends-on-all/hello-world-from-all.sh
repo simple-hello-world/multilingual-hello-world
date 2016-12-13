5951263699cd8a733250e1708113f2cb1b68d662
Stanislav Hadjiiski <hadjiiskis@vmware.com>
2016-12-13 11:21:55 +0200

    Add output to the bash-depends-on-all module's script

--- bash-depends-on-all/hello-world-from-all.sh
+++ bash-depends-on-all/hello-world-from-all.sh
@@ -4,3 +4,5 @@ mkdir ../build
 java -jar ../java-hello-world/target/java-hello-world-1.0.0-SNAPSHOT.jar > ../build/output.txt
 ../go-hello-world/bin/go-hello-world-linux-amd64 >> ../build/output.txt
 #../go-hello-world/bin/go-hello-world-darwin-amd64 >> ../build/output.txt
+echo "==================================================================="
+echo "All done! Build UUID was $BUILD_UUID"
